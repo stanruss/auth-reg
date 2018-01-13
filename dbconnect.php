1753b2e530bfb9a03cc931ec46c549ff98a4a1f3
Stanruss <stan19781@gmail.com>
2018-01-13 16:29:52 +0300

    first commit

---
+++ dbconnect.php
@@ -0,0 +1,27 @@
+<?php
+    // Указываем кодировку
+    header('Content-Type: text/html; charset=utf-8');
+
+    $server = "localhost"; /* имя хоста (уточняется у провайдера), если работаем на локальном сервере, то указываем localhost */
+    $username = "u0343395_stanrus"; /* u0343395_stanrus */
+    $password = "stan818791"; /* stan818791 */
+    $database = "u0343395_u034339"; /* u0343395_u034339 */
+ 
+    // Подключение к базе данных через MySQLi
+    $mysqli = new mysqli($server, $username, $password, $database);
+
+    // Проверяем, успешность соединения. 
+    if (mysqli_connect_errno()) { 
+        echo "<p><strong>Ошибка подключения к БД</strong>. Описание ошибки: ".mysqli_connect_error()."</p>";
+        exit(); 
+    }
+
+    // Устанавливаем кодировку подключения
+    $mysqli->set_charset('utf8');
+
+    //Для удобства, добавим здесь переменную, которая будет содержать название нашего сайта
+    $address_site = "https://poecilotheria.ru/";
+
+    //Почтовый адрес администратора сайта
+    $email_admin = "stan19781@gmail.com";
+?>
